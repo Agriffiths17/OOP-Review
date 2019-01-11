cc8413ecdba412ae65a893c3c136d575f7535f56
Austyn Griffiths <austyng@shaw.ca>
2019-01-11 11:25:12 -0700

    Added comments to program.cs

--- src/CSharp.Language.Review/Program.cs
+++ src/CSharp.Language.Review/Program.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Declares an "area" or named-space in which we can
+// place our programmer-defined data types
 namespace CSharp.Language.Review
 {
+    // The namespace plus the class name is what's called
+    // a "fully-qualified" class name.
+    // The fully-qualified class name for Program is
+    // CSharp.Language.Review.Program
     public class Program
     {
+        // A static field initialized to a new random object
         private static Random rnd = new Random();
 
+        // Main() is the entry point
         public static void Main(string[] args)
         {
+            // The body of the Main() method
+            // acts as the "driver" of my application.
             Program app = new Program(args);
 
             app.AssignMarks(30, 80);
@@ -23,15 +32,23 @@ namespace CSharp.Language.Review
                     System.Console.WriteLine("\t" + item);
             }
         }
-
+        
+        // This field is acting as a "backing store"
+        // for the Students property.
         private List<Student> _students = new List<Student>();
 
+        // This peroperty provides "controlled access"
+        // to the data in the backing store (the field).
         public List<Student> Students
         {
             get { return _students; }
             set { _students = value; }
         }
 
+        // This is a constructor.
+        // The job of a constructor is to ensure
+        // that all of the fields/properties
+        // have "meaningful" values.
         public Program(string[] studentNames)
         {
             WeightedMark[] CourseMarks = new WeightedMark[4];
@@ -50,6 +67,12 @@ namespace CSharp.Language.Review
             }
         }
 
+        /// <summary>
+        /// This assigns a random mark to each student
+        /// in the <see cref="Students"/> property.
+        /// </summary>
+        /// <param name="min">The minimum possible earned value for the student's mark</param>
+        /// <param name="max">The maximum possible earned value for the student's mark</param>
         public void AssignMarks(int min, int max)
         {
             foreach (Student person in Students)
